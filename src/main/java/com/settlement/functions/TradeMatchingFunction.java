d022e25651f7bf66d6eb82da542bbf88079f34a5
rahul sawant <sawant.rahul360@gmail.com>
2026-05-16 13:12:41 +0530

    Handle payment-first trade matching cases

--- src/main/java/com/settlement/functions/TradeMatchingFunction.java
+++ src/main/java/com/settlement/functions/TradeMatchingFunction.java
@@ -11,6 +11,7 @@ import org.apache.flink.configuration.Configuration;
 
 public class TradeMatchingFunction extends KeyedCoProcessFunction<String, TradeOrder, BankPayment, String>{
     private ValueState<TradeOrder> savedOrderState;
+    private ValueState<BankPayment> savedPaymentState;
 
     //    who calls it? The TaskManager (the physical server).
     //    when is it called? Exactly once, right after the TaskManager assigns this code to a CPU slot,
@@ -19,40 +20,57 @@ public class TradeMatchingFunction extends KeyedCoProcessFunction<String, TradeO
     @Override
     public void open(Configuration parameters) {
 //        this is like defining a table in rocksdb
-        ValueStateDescriptor<TradeOrder> descriptor =
+        ValueStateDescriptor<TradeOrder> orderDescriptor =
                 new ValueStateDescriptor<>("trade-order-state", TradeOrder.class);
-        savedOrderState = getRuntimeContext().getState(descriptor);
+        savedOrderState = getRuntimeContext().getState(orderDescriptor);
+
+        ValueStateDescriptor<BankPayment> paymentDescriptor =
+                new ValueStateDescriptor<>("bank-payment-state", BankPayment.class);
+        savedPaymentState = getRuntimeContext().getState(paymentDescriptor);
     }
 
     @Override
     public void processElement1(TradeOrder order, KeyedCoProcessFunction<String, TradeOrder, BankPayment, String>.Context ctx, Collector<String> out) throws Exception {
-        savedOrderState.update(order);
-        long tenSecondsInMillis = 10 * 1000;
-        long deadline = ctx.timerService().currentProcessingTime() + tenSecondsInMillis;
-        ctx.timerService().registerProcessingTimeTimer(deadline);
-        out.collect("RECEIVED ORDER: " + order.getAsset() + " (ID: " + order.getOrderId() + "). Holding in memory. 10-second timer started...");
+        BankPayment savedPayment = savedPaymentState.value();
+        if(savedPayment != null){
+            out.collect("SUCCESS [ORDER LATE]: Settled " + order.getAsset() + " (Order " + order.getOrderId() + ") with status " + savedPayment.getStatus());
+            savedPaymentState.clear();
+        }else {
+            savedOrderState.update(order);
+            long tenSecondsInMillis = 10 * 1000;
+            long deadline = ctx.timerService().currentProcessingTime() + tenSecondsInMillis;
+            ctx.timerService().registerProcessingTimeTimer(deadline);
+            out.collect("RECEIVED ORDER: " + order.getAsset() + " (ID: " + order.getOrderId() + "). Holding in memory. 10-second timer started...");
+        }
     }
 
     @Override
     public void processElement2(BankPayment payment, KeyedCoProcessFunction<String, TradeOrder, BankPayment, String>.Context ctx, Collector<String> out) throws Exception {
         TradeOrder savedOrder = savedOrderState.value();
-
         if (savedOrder != null) {
             out.collect("SUCCESS: Settled " + savedOrder.getAsset() + " (Order " + savedOrder.getOrderId() + ")");
-
             savedOrderState.clear();
-
         } else {
-            out.collect("WARNING: Payment received for ID " + payment.getOrderId() + " but no Order found.");
+            savedPaymentState.update(payment);
+            long deadline = ctx.timerService().currentProcessingTime() + (10 * 1000);
+            ctx.timerService().registerProcessingTimeTimer(deadline);
+            out.collect("RECEIVED PAYMENT: Status " + payment.getStatus() + " (ID: " + payment.getOrderId() + "). Waiting for order...");
         }
     }
 //    a Priority Queue
     @Override
     public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
         TradeOrder stuckOrder = savedOrderState.value();
+        BankPayment stuckPayment = savedPaymentState.value();
+
         if (stuckOrder != null) {
-            out.collect("TIMEOUT ALERT: Payment never arrived for " + stuckOrder.getAsset() + " (Order " + stuckOrder.getOrderId() + "). Cancelling Trade.");
-            savedOrderState.clear();
+            out.collect("TIMEOUT: Order " + stuckOrder.getOrderId() + " expired. Payment never arrived.");
+            savedOrderState.clear(); // free the memory!
+        }
+
+        if (stuckPayment != null) {
+            out.collect("TIMEOUT: Payment " + stuckPayment.getOrderId() + " expired. Order never arrived.");
+            savedPaymentState.clear(); // free the memory!
         }
     }
 }
